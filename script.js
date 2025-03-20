2e28a418802c4aaff9b75dc4d9f555a1e029914c
Ismail Shaikh <91933697+coddyismail@users.noreply.github.com>
2025-03-20 23:03:26 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wishButton = document.querySelector('.wish-button');
 const wishMessage = document.querySelector('.wish-message');
 
 wishButton.addEventListener('click', () => {
-  wishMessage.textContent = "May all your dreams come true, especially the one where you marry your Aahana! 💖";
+  wishMessage.textContent = "May all your dreams come true";
   wishMessage.style.opacity = 1;
 
   // Reset the message after 3 seconds
